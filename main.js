7818ee095b8ac6a520c22c73f43566b61ee18914
Kevin Czaja <czajakevin@gmail.com>
2018-11-21 15:17:44 +0100

    2nd commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// Listener on search input to enable keypress validation
-let track = document.querySelector('#trackname');
+const track = document.querySelector('#trackname');
 
+// Listener on search input to enable keypress validation
 track.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
@@ -8,23 +8,25 @@ track.addEventListener("keydown", function(event) {
     }
 });
 
-// Retrieve and display the search results
 function getTrackList() {
-    document.querySelector('.tracklist-content').innerHTML = '';
-    let track = document.querySelector('#trackname');
+    // Reset the search div
+    document.querySelector('.tracklist').innerHTML = '';
     search(track);
 }
 
+// Retrieve and display search results from deezer API
 const search = function getTrackListFromDeezer(track) {
     fetch(`https://api.deezer.com/search?q=${track.value}&order=RANKING&limit=10`)
     .then(function(response){
         return response.json();
     })
     .then(function(list) {
-        let trackList = '<ul class="search-results">';
-        trackList += '<li class="search-header"><div class="track-cover"></div><span class="track-title">TITLE</span><span class="track-artist">ARTIST</span><span class="track-duration">TIME</span></li>'
+        let trackList = "";
         if(list.data.length > 0)
         { 
+            trackList += `<div class="tracklist-content"><span class="results-text reset-search"><i class="far fa-times-circle"></i>Search : "${track.value}"</span><ul class="search-results">`;
+            trackList += '<li class="search-header"><div class="track-cover"></div><span class="track-title">TITLE</span><span class="track-artist">ARTIST</span><span class="track-duration">TIME</span></li>';
+           
             list.data.forEach(element => {
                 let duration = secToMin(element.duration);
                 trackList += `<li class="search-track" data-id="${element.id}">
@@ -36,15 +38,31 @@ const search = function getTrackListFromDeezer(track) {
                 <source src="${element.preview}" type="audio/mp3" />
                 Votre navigateur n'est pas compatible
                 </audio>
-                <button class="validate" onclick="addToPlaylist(${element.id})";>+</button>
+                <button class="validate" onclick="addToPlaylist(${element.id})";><i class="fas fa-plus"></i></button>
             </li>`;
             }, trackList);
+
+            trackList += '</ul></div>';
+            document.querySelector('.tracklist').innerHTML = trackList;
+
+            let reset = document.querySelector('.reset-search');
+            reset.addEventListener("click", function() {
+                document.querySelector('.tracklist').innerHTML = '';
+            });
+
         }
         else
-            trackList += '<li>Track not found, try something else !</li>';
+            swal({
+                title: "Damn !",
+                text: `Can't find anything about "${track.value}"...`,
+                icon: "error",
+                button: "Alright, let's try something else!",
+            });
+        
+        track.value = "";
+        track.blur();
 
-        trackList += '</ul>';
-        document.querySelector('.tracklist-content').innerHTML = trackList;
+       
     });
 }
 
@@ -63,29 +81,49 @@ const digit = function countDigit(number) {
 }
 
 const addToPlaylist = function addTrackToThePlaylist(trackId) {
-    //console.log(document.querySelector('.playlistContent').innerHTML.length);
     //debugger;
     if(document.querySelector('.playlist').innerHTML.length == 0) {
-        let html = '<div class="playlist-content"><ul class="musicList"></ul></div>';
-        document.querySelector('.playlist').innerHTML = html;                                                             
+        let html = `<div class="playlist-content"><span class="playlist-header" onclick="editTitle();"><i class="fas fa-edit"></i>Whoopadadaboo, your custom playlist :</span>
+        <ul class="musicList"></ul></div>`;
+        document.querySelector('.playlist').innerHTML = html;
+        trackListHeader = '<li class="search-header"><div class="track-cover"></div><span class="track-title">TITLE</span><span class="track-artist">ARTIST</span><span class="track-duration">TIME</span></li>';
+        document.querySelector('.musicList').innerHTML = trackListHeader;
     }
     //récupérer les champs de la chanson correspondant
     var li = document.querySelector('.search-results').querySelector('li[data-id="'+ trackId+'"');
+    var image = li.querySelector('.track-cover').getAttribute('src');
     var title = li.querySelector('.track-title').innerHTML;
     var artist = li.querySelector('.track-artist').innerHTML;
+    var duration = li.querySelector('.track-duration').innerHTML;
+    var preview = li.querySelector('.track-player > source').getAttribute('src');
     var liNumber = document.querySelector('.musicList').querySelectorAll('li').length + 1;
-    var html = `<li data-id="${trackId}">
+    var html = `<li data-id="${trackId}" class="search-track">
+    <img class="track-cover" src="${image}">
     <!--<span class="track-number">${liNumber}</span>-->
     <span class="track-title">${title}</span>
     <span class="track-artist">${artist}</span>
-    <span class="track-duration">Très long</span>
-    <span class="playButton" onclick="playThisMusic">Play</span>
+    <span class="track-duration">${duration}</span>
+    <audio class="track-player" controls="controls">
+    <source src="${preview}" type="audio/mp3" />
+    Votre navigateur n'est pas compatible
+    </audio>
+    <!--<span class="playButton" onclick="playThisMusic">Play</span>-->
+    <button class="delete" onclick="removeFromPlaylist(${trackId})";><i class="fas fa-trash-alt"></i></button>
     </li>`;
     
     //évite les doublons
-    if(document.querySelector('.musicList').querySelector('li[data-id="'+ trackId+'"') == null) {
+    if(document.querySelector('.musicList').querySelector('li[data-id="'+ trackId+'"]') == null) {
         document.querySelector('.musicList').innerHTML += html;
     }
+}
+
+const editTitle = function editPlaylistTitle() {
+    swal("What cool name for your playlist ?", {
+        content: "input",
+      })
+      .then((value) => {
+        
+      });
 }
 
 const playThisMusic = function PlayTrackFromPlaylist(track) {
@@ -94,4 +132,39 @@ const playThisMusic = function PlayTrackFromPlaylist(track) {
 
 const getPlaylist = function getTracksInPlaylist() {
     
+}
+
+const removeFromPlaylist = function removeTrackFromPlaylist(trackId) {
+    swal("This track is dope, you sure ?", {
+        buttons: {
+          no: {
+              text: "No",
+              className: "accept",
+              value: false
+            },
+          confirm: {
+            text: "Yiss !",
+            className: "cancel",
+            value: true
+          }
+      }}).then((value) => {
+          switch(value) {
+              case false:
+                break;
+
+              case true:
+                document.querySelector('.musicList').querySelector('li[data-id="'+ trackId+'"]').remove();
+
+                if(document.querySelector('.musicList').childElementCount === 1) {
+                    document.querySelector('.playlist-content').remove();
+                }
+                break;
+
+          }
+      });
+    }
+
+const exportation = function exportPlaylistToFile() {
+    var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+   saveAs(blob, "filename.txt");
 }
